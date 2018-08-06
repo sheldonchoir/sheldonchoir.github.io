50628ca01c82063f71a16680faf96ced6f459bd2
Biscuit <rjfarruggia@gmail.com>
2018-08-05 18:59:27 -0800

    d

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-function move(){var t=document.getElementById("myBar"),e=1,n=setInterval(function(){35<=e?clearInterval(n):(e++,t.style.width=e+"%")},10)}function move2(){var t=document.getElementById("myBar2"),e=1,n=setInterval(function(){45<=e?clearInterval(n):(e++,t.style.width=e+"%")},10)}function move3(){var t=document.getElementById("myBar3"),e=1,n=setInterval(function(){95<=e?clearInterval(n):(e++,t.style.width=e+"%")},10)}function validate(){"shschoirs2018"==$("#password1").val()?($("#validate-status").text("Password accepted. Redirecting ..."),window.setTimeout(function(){window.location.replace("https://google.com/")},2e3)):$("#validate-status").text("Password invalid.")}$(document).ready(function(){$("#password1").keyup(validate)});
+function move() {
+    var t = document.getElementById("myBar"),
+        e = 1,
+        n = setInterval(function() {
+            35 <= e ? clearInterval(n) : (e++, t.style.width = e + "%");
+        }, 10);
+}
+
+function move2() {
+    var t = document.getElementById("myBar2"),
+        e = 1,
+        n = setInterval(function() {
+            45 <= e ? clearInterval(n) : (e++, t.style.width = e + "%");
+        }, 10);
+}
+
+function move3() {
+    var t = document.getElementById("myBar3"),
+        e = 1,
+        n = setInterval(function() {
+            95 <= e ? clearInterval(n) : (e++, t.style.width = e + "%");
+        }, 10);
+}
+
+function validate() {
+    "shschoirs2018" == $("#password1").val() ? ($("#validate-status").text("Password accepted. Redirecting ..."), window.setTimeout(function() {
+        window.location.replace("https://youtu.be/J1TYgWhVQPU");
+    }, 2e3)) : $("#validate-status").text("Password invalid.");
+}
+$(document).ready(function() {
+    $("#password1").keyup(validate);
+});
